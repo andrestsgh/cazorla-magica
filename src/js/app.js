25b9e6d2a7e1feb858fd5bae64f1e3bece0f6c20
Andrés Terol Sánchez <andrests78@gmail.com>
2024-11-15 16:41:16 +0100

    Herramientas1. PEC1. Cazorla Mágica. Finished. BEM naming+ITCSS structure. Added SASS, some mixins and simple javascript code.

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,15 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+/* Need to set main marginTop for the fixed header responsive */
+
+/* Event on content loaded */
+window.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
+  const main = document.querySelector('.main-content');
+  main.style.marginTop = `${header.offsetHeight}px`;
+});
+
+/* Event on resized window */
+window.addEventListener('resize', () => {
+  const header = document.querySelector('.header');
+  const main = document.querySelector('.main-content');
+  main.style.marginTop = `${header.offsetHeight}px`;
+});
